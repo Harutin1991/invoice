1ee73d8a809a4fbd787779488b3ed4dafc84e42c
Harut <harut.soghomonyan@gmail.com>
2021-04-06 16:13:51 +0400

    Rout fix

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ $router->group(['prefix'=>'api/v1'], function() use($router){
 
     $router->get('/item', 'InvoiceController@index');
     $router->get('/item/{id}', 'InvoiceController@getInvoiceItem');
-    $router->get('/invocie-status', 'InvoiceController@getInvoiceStatuses');
+    $router->get('/invoice-status', 'InvoiceController@getInvoiceStatuses');
     $router->get('/payment-status', 'InvoiceController@getPaymentStatuses');
     $router->get('/payment-type', 'InvoiceController@getPaymentType');
     $router->get('/invoice-type', 'InvoiceController@getInvoiceType');
